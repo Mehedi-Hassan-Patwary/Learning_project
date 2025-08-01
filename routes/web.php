30c99d898670ff4b97b5f78870648f0d5c979040
MD Mehedi Hassan <160578251+Mehedi-Hassan-Patwary@users.noreply.github.com>
2025-08-01 11:29:50 +0600

    Route parameters

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/post', function () {
-    return view('post');
-});
+// Route::get('/post', function () {
+//     return view('post');
+// });
+
+// Route::get('/post/{id?}/comment/{commentId?}', function (string $id = null, string $commentId = null){
+//         if($id){
+//             return "<h1>Post ID : ".$id ."</h1><h2>Comment ID : ".$commentId."</h2>";
+//         }else{
+//             return "<h1>Post ID not found</h1>";
+//         }
+//     // return view('post');
+//     // return view('post', ['id' => $id]);
+
+
+// });
+
+Route::get('/post/{id?}', function (string $id = null) {
+    if($id){
+        return "<h1>Post ID : ".$id ."</h1>";
+    }else{
+        return "<h1>Post ID not found</h1>";
+    }
+})->whereAlphaNumeric('id');
+
